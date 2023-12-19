b4e504bded6d634052a6bfbf66a0fed5a14aa43b
thecattest <vodopyanov999@gmail.com>
2023-12-19 18:22:23 +0300

    initial

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse, HTMLResponse
+from os import popen
+
+app = FastAPI()
+favicon_path = 'poweroff.ico'
+
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    return FileResponse(favicon_path)
+
+
+@app.get('/', response_class=HTMLResponse)
+async def root():
+    # a = popen('ls').read()
+    return '''
+    <div style="display: flex;justify-content: center;align-items: center;height: 100%;">
+        <a href="/poweroff"><img src="favicon.ico"></a>
+    </div>
+    '''
+
+
+@app.get('/poweroff')
+async def root():
+    # return popen('ls').read()
+    return popen('shutdown now --reboot').read()
+    # return popen('shutdown now -H').read()
+    # return popen('shutdown now --poweroff').read()
